65613ddd6632a6d21032972abc61ea9fbd5c7973
alaw22 <lawsonalex1999@gmail.com>
2025-03-22 22:12:19 -0500

    Added game constants

---
+++ constants.py
@@ -0,0 +1,27 @@
+# ASCII Patterns
+HEAD_UP = "''"
+HEAD_RIGHT = " :"
+HEAD_LEFT = ": "
+HEAD_DOWN = ".."
+
+BACK_HORIZONTAL = "=="
+BACK_VERTICAL = chr(166)*2
+
+TOP_RIGHT = chr(175) + "|"
+TOP_LEFT = "|" + chr(175)
+BOTTOM_RIGHT = "_|"
+BOTTOM_LEFT = "|_"
+
+# Colors
+SNAKE_C = "0;128;0"
+EYES_C = "255;192;203"
+PATTERN_C = "255;255;255"
+APPLE_C = "228;7;0"
+BACKGROUND_C = "0;0;0"
+
+# Escape Sequence
+ESC = "\x1b["
+
+# Game board constants
+HEIGHT = 25
+WIDTH = 25
